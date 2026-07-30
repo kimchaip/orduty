00f39d8568b331f26f6e163b8d59db375886c25c
kimchaip <kim_chaip@hotmail.com>
2026-07-30 22:31:59 +0700

    build schedule

--- app/admin/tabs/calendar/components/CalendarCell.tsx
+++ app/admin/tabs/calendar/components/CalendarCell.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CalendarDay } from "../types";
+import Link from "next/link";
 
 interface CalendarCellProps {
   day: CalendarDay | null;
@@ -14,20 +15,22 @@ export default function CalendarCell({ day }: CalendarCellProps) {
   const isHoliday = day.is_holiday;
 
   return (
-    <div
-      className={`border h-24 p-2 rounded cursor-pointer transition-colors
+    <Link href={`/admin/tabs/schedule?date_id=${day.id}`}>
+      <div
+        className={`border h-24 p-2 rounded cursor-pointer transition-colors
         ${isHoliday ? "bg-rose-50 hover:bg-rose-100" : "bg-white hover:bg-gray-100"}
       `}
-    >
-      <div className="font-bold text-gray-800">
-        {new Date(day.date).getDate()}
-      </div>
-
-      {day.holiday_name && (
-        <div className="text-xs text-rose-700 font-bold mt-1">
-          {day.holiday_name}
+      >
+        <div className="font-bold text-gray-800">
+          {new Date(day.date).getDate()}
         </div>
-      )}
-    </div>
+
+        {day.holiday_name && (
+          <div className="text-xs text-rose-700 font-bold mt-1">
+            {day.holiday_name}
+          </div>
+        )}
+      </div>
+    </Link>
   );
 }
